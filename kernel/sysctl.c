acce00a5e8157253385faf85cf71e5b38d5ce136
diudiuyc5200 <17766728994@163.com>
2026-02-14 20:07:20 +0800

    Update sysctl.c

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1567,7 +1567,7 @@ static struct ctl_table vm_table[] = {
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= &zero,
+		.extra1		= &sixty,
 		.extra2		= &two_hundred,
 	},
 	{
